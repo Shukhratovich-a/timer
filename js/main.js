2b793d794708a7fb4bd19e7c83a451cf30f26f24
Shukhratovich-a <shukhratovich138@gmail.com>
2022-02-20 03:33:53 +0500

    Edit timer

--- js/main.js
+++ js/main.js
@@ -1,49 +1,48 @@
+const elTimerNode = selectElement(".timer");
 const elTimerButton = selectElement(".footer__button");
-const elTimerCircleNode = selectElement(".timer__circle");
 const elTimer = selectElement(".timer__aside__time");
+const elTimerCircleNode = selectElement(".timer__circle");
+const elTimerFirst = selectElement('.timer__clock__number[aria-label="first"]');
+const elTimerSecond = selectElement('.timer__clock__number[aria-label="second"]');
 
-let isPaused = false;
+let firstTime = 0;
+let secondTime = 0;
 
-elTimerButton.addEventListener("click", () => {
-  timerMode(elTimerButton);
+const setTime = (button, time, property) => {
+  if (time === 0 && button.name === property + "-bottom") time = 59;
+  else if (time === 59 && button.name === property + "-top") time = 0;
+  else {
+    if (button.name === property + "-top") time++;
+    else if (button.name === property + "-bottom") time--;
+  }
+  return time;
+};
+
+const setText = () => {
+  elTimerFirst.textContent = String(firstTime).padStart(2, 0);
+  elTimerSecond.textContent = String(secondTime).padStart(2, 0);
+};
+
+const normalizeTime = () => {
+  if (secondTime === -1) firstTime--, (secondTime = 59);
+};
+
+elTimer.addEventListener("click", (evt) => {
+  const timerButton = evt.target;
+  firstTime = setTime(timerButton, firstTime, "first");
+  secondTime = setTime(timerButton, secondTime, "second");
+  setText();
 });
 
-const timerInterval = (secunds) => {
-  const interval = setInterval(() => {
-    if (!isPaused) {
-      if (secunds <= 0) {
-        clearInterval(interval);
-        elTimerButton.classList.remove("footer__button--active");
-        elTimerCircleNode.classList.remove("timer__circle--active");
-      } else {
-        secunds--;
-        elTimer.value = secunds;
-      }
-    }
-  }, 1000);
-};
-
-const animationOptions = (time) => {
-  elTimerCircleNode.style.animationDuration = time + "s, " + 0.2 + "s";
-  elTimerCircleNode.style.animationDelay = 0 + "s, " + time + "s";
-};
-
-const timerMode = (button) => {
-  let secunds = Number(elTimer.value.trim());
-  if (!button.matches(".footer__button--active")) {
-    isPaused = false;
-    elTimer.value = secunds;
-    elTimerButton.classList.add("footer__button--active");
-    elTimerCircleNode.classList.remove("timer__circle--paused");
-    button.classList.add("footer__button--active");
-    elTimerCircleNode.classList.add("timer__circle--active");
-    animationOptions(secunds);
-
-    timerInterval(secunds);
-  } else {
-    isPaused = true;
-    timerInterval(secunds);
-    elTimerCircleNode.classList.add("timer__circle--paused");
-    button.classList.remove("footer__button--active");
+elTimerButton.addEventListener("click", () => {
+  if (firstTime > 0 || secondTime > 0) {
+    const interval = setInterval(() => {
+      secondTime--;
+      if (firstTime === 0 && secondTime === 0) clearInterval(interval);
+      normalizeTime();
+      setText();
+    }, 1000);
   }
-};
+
+  elTimerNode.classList.add("timer--active");
+});
